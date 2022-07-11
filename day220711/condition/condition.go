418d746472fce62a02a165d30dd5dc8cd8f99e0b
wudg <321073229@qq.com>
2022-07-11 15:10:14 +0800

    solve conflict

--- day220711/condition/condition.go
+++ day220711/condition/condition.go
@@ -39,12 +39,12 @@ func main() {
 	var i1, i2 int
 	select {
 	case i1 = <-c1:
-		fmt.Printf("received ", i1, " from c1\n")
+		fmt.Println("received ", i1, " from c1")
 	case c2 <- i2:
-		fmt.Printf("sent ", i2, " to c2\n")
+		fmt.Println("sent ", i2, " to c2")
 	case i3, ok := (<-c3): // same as: i3, ok := <-c3
 		if ok {
-			fmt.Printf("received ", i3, " from c3\n")
+			fmt.Println("received ", i3, " from c3")
 		} else {
 			fmt.Printf("c3 is closed\n")
 		}
